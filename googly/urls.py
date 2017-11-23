b272aeb018ac8ac37b664604f2cc32bf7a5a67d6
fsalsayegh <as.alsayegh16@gmail.com>
2017-11-23 20:55:10 +0300

    map and place location

--- googly/urls.py
+++ googly/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^psearch/$', views.place_text_search, name='place-search'),
+    url(r'^pdetail/$', views.place_detail, name='place-detail')
     
 
     ]
--- googly/views.py
+++ googly/views.py
@@ -3,16 +3,29 @@ from django.shortcuts import render
 import requests
 
 def place_text_search(request):
-	key= "AIzaSyCS0JKWzwh49JM_6xetUZOTQlQvKlhrLIQ"
-	query = "Coded"
+	key= "AIzaSyBGaugK8KUB2-t4QShjpOs_M1B3s2i6MHk"
+	query = request.GET.get( "query" , "Coded")
 	url ="https://maps.googleapis.com/maps/api/place/textsearch/json?query=%s&region=kw&key=%s"%(query, key)
 
+	next_page= request.GET.get('nextpage')
+	if next_page is not None:
+		url+="&pagetoken"+next_page
+	response = requests.get(url)
+	context={'response':response.json()}
+	return render(request, "place_search.html", context)
+	#return JsonResponse(response.json(), safe=False)
+
+
+def place_detail(request):
+	key="AIzaSyBGaugK8KUB2-t4QShjpOs_M1B3s2i6MHk"
+	place_id=request.GET.get('place_id','')
+
+	url = "https://maps.googleapis.com/maps/api/place/details/json?key=%s&placeid=%s"%(key, place_id)
+	map_key = "AIzaSyCrqwdrlrwhfGK9mlZPVFoWC7RTq_AmSJ4"
 
 	response = requests.get(url)
-	context = {"response": response.json()}
-	return render(request, "place_search.html", context)
-		
-	#return JsonResponse(response.json(), safe=False)
+	context={'response':response.json() , "map": map_key}
+	return render(request, 'place_detail.html', context)
 
 
 
